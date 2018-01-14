8f84a5f31bb9175365013a74bad202f1a39612c8
Roy Barak <roy.barak@tcwd.fr>
2018-01-14 17:14:17 +0200

    First version

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+    return (
+        <Header>
+            <Link to="/">Music DB</Link>
+        </Header>
+    )
+
+}
+
+export default Header;
